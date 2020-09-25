cee1f2180eb8aa06b0417a13eab7dd97eed6ed65
Leekwangsoo <jayzisgodminion@gmail.com>
2020-09-25 16:30:44 +0900

    new_commit_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp()); /*1*/
 
-class MyApp extends StatelessWidget {/*2*/
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { /*3*/
-    return MaterialApp(  /*4*/
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold( /*5*/
-        appBar: AppBar( /*6*/
+      home: Scaffold(
+        appBar: AppBar(
           title: Text('Welcome to Flutter'),
         ),
-        body: Center( /*7*/
-          child: Text('Hello World'),
+        body: Center(
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
