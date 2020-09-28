31f9fb0be3b43aa7a31bebd0bcb76185cc327a72
Leekwangsoo <jayzisgodminion@gmail.com>
2020-09-28 22:25:14 +0900

    基本的なウィジェット

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,19 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
+  String _name;
   Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home:Text(
-        'Heeeeelloooo,  how are you?',
-        textAlign: TextAlign.center,
-        overflow: TextOverflow.ellipsis,
-        style:TextStyle(fontWeight: FontWeight.bold),
-      )
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home:Text(
+          'Hee, $_name! how are you?',
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style:TextStyle(fontWeight: FontWeight.bold),
+        )
     );
   }
 }
