5b1ae59e88fbc79e38ef9868b3adfde38630c585
Leekwangsoo <jayzisgodminion@gmail.com>
2020-10-06 23:28:07 +0900

    firebase collection取得完了

--- lib/book_list_page.dart
+++ lib/book_list_page.dart
@@ -9,11 +9,18 @@ class BookList extends StatelessWidget {
         title: Text('本一覧'),
       ),
       body: StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance.collection('books').snapshots(),
+        //疑問、streambuilderとは何だろうか？
+        //collectionのbooksが、Firestore上のコレクション名にあたる。
+        //stream: Firestore.instance.collection('books').snapshots(),
+        //以下のコードでは、addressを見にいく。
+        //ちなみに、addressの中身が空白の場合は空白が帰ってくる模様。
+        stream: Firestore.instance.collection('address').snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           return ListView(
             children: snapshot.data.documents.map((DocumentSnapshot document) {
+              //documentが一つ一つのレコードかな？
               return ListTile(
+                //documentのtitleで、カラム名を指定している。
                 title: Text(document['title']),
               );
             }).toList(),
